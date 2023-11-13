14975b375055a1b5489c826964af893a7bd80d35
nvnguyen2504 <nvnguyen2504@gmail.com>
2023-11-13 14:31:34 +0700

    menu

--- client/src/app/Components/menu-left/menu-left.component.ts
+++ client/src/app/Components/menu-left/menu-left.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseApiUrl } from 'src/app/general';
 
 @Component({
   selector: 'app-menu-left',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-left.component.scss']
 })
 export class MenuLeftComponent {
+  constructor() {
+   
+  }
 
+
+  ver: string = JSON.stringify(localStorage.getItem("ver"));
+  links = [
+    {
+      text: "Trang Chủ",
+      link: `/${BaseApiUrl.BaoCaos}`,
+      icon: "home",
+    },
+    {
+      text: "Đơn Hàng",
+      link: `/${BaseApiUrl.DonHangs}`,
+      icon: "shopping_basket",
+     }
+     //,{
+    //   text:'Đơn Hàng',
+    //   link:`/${BaseApiUrl.listOrders}`,
+    //   icon: "shopping_basket",
+    // }
+    ,{
+      text: "Sản Phẩm",
+      link:  `/${BaseApiUrl.SanpPhams}`,
+      icon: "spa",
+    },
+    {
+      text: "Nhập  - Tồn Kho",
+      link:  `/${BaseApiUrl.NhapHangs}`,
+      icon: "credit_card",
+    },
+    {
+      text: "Khách Hàng",
+      link:  `/${BaseApiUrl.KhachHangs}`,
+      icon: "account_box",
+    },
+    {
+      text: "Chi Phí",
+      link:  `/${BaseApiUrl.ChiPhis}`,
+      icon: "money",
+    },
+    {
+      text: "Công Nợ",
+      link:  `/${BaseApiUrl.CongNos}`,
+      icon: "monetization_on",
+    },
+    {
+      text: "Cài Dặt",
+      link: "/settings",
+      icon: "settings",
+    },
+  ];
 }
